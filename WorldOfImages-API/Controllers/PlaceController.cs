79b46a852b8e6065d73a9fc089a2be26f6bf55ba
Kamil Zegadlo <kamil.zegadlo@easyjet.com>
2018-11-16 19:49:50 +0000

    Adding almost empty API projects with tests. Starting from PlaceControllerTests

---
+++ WorldOfImages-API/Controllers/PlaceController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WorldOfImagesAPI.ValueObjects;
+
+namespace WorldOfImages_API.Controllers
+{
+    [Route("api/[controller]")]
+    public class PlaceController : Controller
+    {
+        [HttpGet("{id}")]
+        public string Get(GetPlaceRequest getPlaceRequest)
+        {
+            return "fixedPlace";
+        }
+    }
+}
---
+++ WorldOfImages-APITest/PlaceControllerTest.cs
@@ -0,0 +1,24 @@
+using WorldOfImages_API.Controllers;
+using WorldOfImagesAPI.ValueObjects;
+using Xunit;
+
+namespace WorldOfImages_APITest
+{
+    public class PlaceControllerTest
+    {
+        private readonly PlaceController placeController = new PlaceController();
+
+        [Fact]
+        public void Get_Place_ShouldReturnFixedValue()
+        {
+            //arrange
+            var getPlaceRequest = new GetPlaceRequest(1, 2);
+
+            //act
+            var result = placeController.Get(getPlaceRequest);
+
+            //assert
+            Assert.Equal("fixedPlace", result);
+        }
+    }
+}
