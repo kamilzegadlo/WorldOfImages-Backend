65333efb75158638ea1e75fe58f6f5606df611d1
kamilzegadlo <kamilzegadlo@gmail.com>
2018-11-17 18:46:01 +0000

    PlaceController starts using PlaceRepository

--- WorldOfImages-API/Controllers/PlaceController.cs
+++ WorldOfImages-API/Controllers/PlaceController.cs
@@ -1,15 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using WorldOfImagesAPI.ValueObjects;
 using WorldOfImagesAPI.DomainEntities;
+using WorldOfImagesAPI.Repositories;
 
 namespace WorldOfImagesAPI.Controllers
 {
     [Route("api/[controller]")]
     public class PlaceController : Controller
     {
+        private readonly IPlaceRepository _placeRepository;
+
+        public PlaceController(IPlaceRepository placeRepository)
+        {
+            _placeRepository = placeRepository;
+        }
+
         public Place Get(Coordinates getPlaceRequest)
         {
-            return new Place(getPlaceRequest.x, getPlaceRequest.y, "unit test name");
+            return _placeRepository.GetPlace(getPlaceRequest);
         }
     }
 }
--- WorldOfImages-APITest/PlaceControllerTest.cs
+++ WorldOfImages-APITest/PlaceControllerTest.cs
@@ -2,28 +2,39 @@ using WorldOfImagesAPI.Controllers;
 using WorldOfImagesAPI.ValueObjects;
 using WorldOfImagesAPI.DomainEntities;
 using Xunit;
+using WorldOfImagesAPI.Repositories;
+using FakeItEasy;
 
 namespace WorldOfImages_APITest
 {
     public class PlaceControllerTest
     {
-        private readonly PlaceController placeController = new PlaceController();
+        private readonly IPlaceRepository _placeRepository;
+        private readonly PlaceController _placeController;
+
+        public PlaceControllerTest()
+        {
+            _placeRepository = A.Fake<IPlaceRepository>();
+
+            _placeController = new PlaceController(_placeRepository);
+        }
 
         [Fact]
-        public void Get_Place_ShouldReturnFixedValue()
+        public void Get_Place_ShouldCallPlaceRepository()
         {
             //arrange
             var getPlaceRequest = new Coordinates(1, 2);
+            var place = new Place(2, 3, "unit test name");
+            A.CallTo(() => _placeRepository.GetPlace(getPlaceRequest)).Returns(place);
 
             //act
-            var result = placeController.Get(getPlaceRequest);
+            var result = _placeController.Get(getPlaceRequest);
 
             //assert
             Assert.IsType(typeof(Place), result);
-            Assert.Equal(1, result.x);
-            Assert.Equal(2, result.y);
+            Assert.Equal(2, result.x);
+            Assert.Equal(3, result.y);
             Assert.Equal("unit test name", result.name);
-
             Assert.Equal(0, result.images.Count);
         }
 
