b28ab709ba8e59deb8b1616d35149d7679e8c810
kamilzegadlo <kamilzegadlo@gmail.com>
2018-12-19 23:29:38 +0000

    Adding messages to sys.messages.

---
+++ WorldOfImages_Database/Scripts/Post-Deploy/PostDeploy_v1_0.sql
@@ -0,0 +1,11 @@
+﻿IF EXISTS (SELECT 1 FROM sys.messages WHERE message_id=50001)
+BEGIN
+	EXEC sp_dropmessage 50001
+END
+EXEC sp_addmessage 50001, 16, N'A place with the provided coordinates alredy exists.'
+
+IF EXISTS (SELECT 1 FROM sys.messages WHERE message_id=50002)
+BEGIN
+	EXEC sp_dropmessage 50002
+END
+EXEC sp_addmessage 50002, 16, N'An image with the given id alredy exists.'
